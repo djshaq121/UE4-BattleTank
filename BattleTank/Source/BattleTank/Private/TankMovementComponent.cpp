de7a3d9e69e0f14fa8dfc6212518b7aa5af49cf4
Shaquille <shaquille-etienne@hotmail.com>
2016-08-03 23:01:33 +0100

    The AI Tanks can move forward and backwards until the tabk is parallel with the player tank

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -16,9 +16,14 @@ void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
 	//No reason to call super because we are replacing the functionality
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s Vectoring to %s"), *TankName, *MoveVelocityString)
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();//Gets the x vector of the tanks 
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();//safe normal, gets you a normal vertor of of this move velocity with out changing it
+	
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention); 
+	IntendMoveForward(ForwardThrow);
+
+														   
+														   //UE_LOG(LogTemp, Warning, TEXT("%s Vectoring to %s"), *TankName, *MoveVelocityString)
 }
 
 //This method doesnt requre anothr method to move back back the analog stick can give a negatibe number which will reverse the tank 
